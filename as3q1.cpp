5fac9ae32f94e74097c2f8953c0626ebdd81945a
utkarsh-g19 <utkarsham825@gmail.com>
2025-04-28 21:55:44 +0530

    Update as3q1.cpp

--- as3q1.cpp
+++ as3q1.cpp
@@ -1,28 +1,10 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
-
-    int nums[n];
-    cout << "Enter array elements: "<< endl;
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i]=nums[i-1] + nums[i];
+        }
+        return nums;
     }
-
-    int runningSum[n];
-    runningSum[0] = nums[0];
-
-    for (int i = 1; i < n; i++) {
-        runningSum[i] = runningSum[i-1] + nums[i];
-    }
-
-    cout << "Running Sum: ";
-    for (int i = 0; i < n; i++) {
-        cout << runningSum[i] << " ";
-    }
-
-    return 0;
-}
+    
+};
